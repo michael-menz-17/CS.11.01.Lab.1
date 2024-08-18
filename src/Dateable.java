ee174f5e4fcac911c3eee48eebf72b49bdd44871
techarenz <57818506+techarenz@users.noreply.github.com>
2024-08-19 06:59:55 +0800

    Update README.md

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+
+        Scanner agescan = new Scanner(System.in);
+        System.out.println("Please enter your age: ");
+
+        int age =-1;
+        while (age < 0){
+            try {
+                age = Integer.parseInt(agescan.nextLine());
+            }
+            catch(NumberFormatException e)
+            {
+                System.out.println("Please enter a valid number");
+            }
+        }
+
+
+        int minDateAge = (age / 2) + 7;
+        System.out.println("You should date somebody who is at least " + minDateAge + "  years old.");
+
     }
 }
